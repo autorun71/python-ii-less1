cb285ea81300e4976524d7297b847e3782a2780f
Евгений Расторгуев <autorun71@mail.ru>
2018-12-04 23:13:54 +0300

    #

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -3,8 +3,9 @@ from keras.models import Sequential
 
 from keras.layers import Dense
 from keras.utils import np_utils
-# asdasd 
-# 333
 
+#задаем seed для повторяемости
+numpy.random.seed(42)
 
-
+# Загружаем данные
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
